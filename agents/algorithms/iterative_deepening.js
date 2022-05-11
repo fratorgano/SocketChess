64090d96e5d134ecbf2156739ffcd4fa8de106e9
Francesco Torgano <francesco.torgano@outlook.it>
2022-05-11 18:27:42 +0200

    Add iterative deepening option

---
+++ agents/algorithms/iterative_deepening.js
@@ -0,0 +1,13 @@
+const { iterative_deepening } = require('wasm-chess-algorithms');
+
+function chooseMove(fen, options, _lastResult) {
+  const move = iterative_deepening(fen, BigInt(1), BigInt(options.time));
+  console.log(move);
+
+  return {
+    mov: move,
+  };
+}
+module.exports = {
+  chooseMove,
+};
--- agents/worker.js
+++ agents/worker.js
@@ -5,6 +5,7 @@ const negamax = require('./algorithms/negamax');
 const negamaxAB = require('./algorithms/negamax_a_b');
 const negamaxABTable = require('./algorithms/negamax_a_b_table');
 const negamaxABQuiescent = require('./algorithms/negamax_a_b_quiescent');
+const iterativeDeepening = require('./algorithms/iterative_deepening');
 
 workerpool.worker({
   random: random.chooseMove,
@@ -12,4 +13,5 @@ workerpool.worker({
   negamax_a_b: negamaxAB.chooseMove,
   negamax_a_b_table: negamaxABTable.chooseMove,
   negamax_a_b_quiescent: negamaxABQuiescent.chooseMove,
+  iterative_deepening: iterativeDeepening.chooseMove,
 });
