215721e0efd249d9f6b9f2f428a6eb061e004070
Francesco Torgano <francesco.torgano@outlook.it>
2022-05-13 16:22:00 +0200

    Add ID option with move ordering

--- agents/worker.js
+++ agents/worker.js
@@ -6,6 +6,7 @@ const negamaxAB = require('./algorithms/negamax_a_b');
 const negamaxABTable = require('./algorithms/negamax_a_b_table');
 const negamaxABQuiescent = require('./algorithms/negamax_a_b_quiescent');
 const iterativeDeepening = require('./algorithms/iterative_deepening');
+const iterativeDeepeningOrder = require('./algorithms/iterative_deepening_order');
 
 workerpool.worker({
   random: random.chooseMove,
@@ -14,4 +15,5 @@ workerpool.worker({
   negamax_a_b_table: negamaxABTable.chooseMove,
   negamax_a_b_quiescent: negamaxABQuiescent.chooseMove,
   iterative_deepening: iterativeDeepening.chooseMove,
+  iterative_deepening_order: iterativeDeepeningOrder.chooseMove,
 });
